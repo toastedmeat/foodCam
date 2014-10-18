1ff682718f71a3f8e5d9267614f55823d5e4f2de
toastedmeat <eloo12@gmail.com>
2014-10-18 18:13:07 -0400

    Added Splash class
    jfjdlkfj

---
+++ src/com/foodCamHack/foodcam/Splash.java
@@ -0,0 +1,14 @@
+package com.foodCamHack.foodcam;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Splash extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+}
