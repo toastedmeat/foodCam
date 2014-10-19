c9eb8cb1e45b56765e0729520b0dce0007a7d5b3
Jessica <jessicazengg@gmail.com>
2014-10-18 22:28:11 -0400

    Camera

--- src/com/foodCamHack/foodcam/camera.java
+++ src/com/foodCamHack/foodcam/camera.java
@@ -1,15 +1,24 @@
 package com.foodCamHack.foodcam;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class camera extends Activity {
-
+	ImageView imgFavorite;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.camera_screen);
+		open();
 	}
-
+	
+	public void open(){
+		Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+	    startActivityForResult(intent, 0);
+	}
 }
