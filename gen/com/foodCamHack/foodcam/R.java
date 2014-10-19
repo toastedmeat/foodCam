bbf4be226674301396b4b81f901c15fb425b7ab5
toastedmeat <eloo12@gmail.com>
2014-10-18 20:51:55 -0400

    Menu Buttons
    Added three menu buttons

--- gen/com/foodCamHack/foodcam/R.java
+++ gen/com/foodCamHack/foodcam/R.java
@@ -1106,10 +1106,13 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_settings=0x7f050040;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int button1=0x7f05003c;
+        public static final int button2=0x7f05003d;
+        public static final int button3=0x7f05003e;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
@@ -1151,7 +1154,7 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textView1=0x7f05003c;
+        public static final int textView1=0x7f05003f;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1258,6 +1261,9 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
+        public static final int camerabutton=0x7f0a0012;
+        public static final int discoverbutton=0x7f0a0011;
+        public static final int explorebutton=0x7f0a0010;
         public static final int title=0x7f0a000e;
     }
     public static final class style {
