373dc454ee93e932d7fd143d43483c922a31f6ac
Jessica <jessicazengg@gmail.com>
2014-10-19 02:06:31 -0400

    Camera Init work.

--- src/com/foodCamHack/foodcam/camera.java
+++ src/com/foodCamHack/foodcam/camera.java
@@ -2,16 +2,69 @@ package com.foodCamHack.foodcam;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 public class camera extends Activity {
 
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB) @Override
+	private Camera cam = null;
+	private ShowCamera showCam;
+
+	public static Camera isCamAvail() {
+		Camera Object = null;
+		try {
+			Object = Camera.open();
+		} catch (Exception e) {
+
+		}
+		return Object;
+	}
+
+	private PictureCallback capturedIt = new PictureCallback() {
+
+		@Override
+		public void onPictureTaken(byte[] data, Camera camera) {
+
+			Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+			if (bitmap == null) {
+				Toast.makeText(getApplicationContext(), "not taken",
+						Toast.LENGTH_SHORT).show();
+			} else {
+				Toast.makeText(getApplicationContext(), "taken",
+						Toast.LENGTH_SHORT).show();
+			}
+			cam.release();
+		}
+	};
+
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.camera_screen);
+		cam = isCamAvail();
+		cam.setDisplayOrientation(90);
+		showCam = new ShowCamera(this, cam);
+		FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+		preview.addView(showCam);
 	}
 
+	public void snapIt(View view) {
+		cam.takePicture(null, null, capturedIt);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 }
