15fe12b29b8799cd4fc7576c311bbcbfdc20cad0
toastedmeat <eloo12@gmail.com>
2014-10-18 21:24:13 -0400

    jesss

---
+++ src/com/foodCamHack/foodcam/explore.java
@@ -0,0 +1,15 @@
+package com.foodCamHack.foodcam;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class explore extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.explore_screen);
+	}
+	
+}
